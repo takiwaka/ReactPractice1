46f3d236a78e93e520b8203837e468958ae4457d
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-10-20 18:00:56 +0900

    state学習中

--- src/Components/Complete.jsx
+++ src/Components/Complete.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Complete = () => {
+  const onClickButton = () => alert();
   return (
     <div>
       <h2>完了したTODO</h2>
       <p>TODOその１</p>
-      <button>戻す</button>
+      <button onClick={onClickButton}>戻す</button>
       <button>削除</button>
     </div>
   );
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Input = () => {
+  const onClickButton = () => {
+    setNum(10000);
+  };
+  const [num, setNum] = useState(0);
   return (
     <div>
       <input type="text" placeholder="TODO入力" />
-      <button>追加</button>
+      <button onClick={onClickButton}>追加</button>
+      <p>{num}</p>
     </div>
   );
 };
