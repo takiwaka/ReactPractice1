c9c7f2b9e0798df16e7f91407b17c0a4ee338cac
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-10-21 05:44:58 +0900

    state,useEffect習得

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
 
 const Input = () => {
-  const onClickButton = () => {
-    setNum(10000);
-  };
   const [num, setNum] = useState(0);
+  const [faceShow, setFaceshow] = useState(true);
+
+  const onClickButton = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwich = () => {
+    setFaceshow(!faceShow);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShow || setFaceshow(true);
+      } else {
+        faceShow && setFaceshow(false);
+      }
+    }
+  }, [num]);
+
   return (
     <div>
       <input type="text" placeholder="TODO入力" />
       <button onClick={onClickButton}>追加</button>
+      <button onClick={onClickSwich}>ON/OFF</button>
       <p>{num}</p>
+      {faceShow && <p>( ^ω^ )</p>}
     </div>
   );
 };
