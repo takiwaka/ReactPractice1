b8b2c4737c641bcb51040b810d88f981e9ce99e0
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-10-22 17:10:45 +0900

    html入力中

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
 import React from "react";
-import Input from "./Components/Input";
-import Incomplete from "./Components/Incomplete";
-import Complete from "./Components/Complete";
 
-const App = () => {
+export const App = () => {
+  const wrapStyle = {
+    backgroundColor: "#c1ffff",
+    minHeight: "100vh"
+  };
+
+  const titleStyle = {
+    margin: "0",
+    padding: "50px",
+    textAlign: "center"
+  };
+
   return (
-    <>
-      <Input />
-      <Incomplete />
-      <Complete />
-    </>
+    <div style={wrapStyle}>
+      <h1 style={titleStyle}>T</h1>
+
+      <div>↑</div>
+
+      <div className="input-area">
+        <textarea
+          placeholder="つぶ"
+          name=""
+          id=""
+          cols="40"
+          rows="5"
+        ></textarea>
+        <button>つぶ〇く</button>
+        <p className="count">140/150</p>
+      </div>
+
+      <div>
+        <ul>
+          <li>
+            <img src="" alt="" />
+            <div className="delete">×</div>
+            <p>tubuyaitanaiyou</p>
+            <p>10:30</p>
+          </li>
+          <li>
+            <div className="delete">×</div>
+            <p>tubuyaitanaiyou</p>
+            <p>10:30</p>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
-
-export default App;
